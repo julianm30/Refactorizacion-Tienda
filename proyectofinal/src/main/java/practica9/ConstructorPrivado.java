ba525167e1cf4d435400390464014f2200bdaabd
julianm30 <jmoesp768@g.educaand.es>
2026-02-17 12:49:12 +0100

    Clase singleton

---
+++ proyectofinal/src/main/java/practica9/ConstructorPrivado.java
@@ -0,0 +1,5 @@
+package practica9;
+
+public @interface ConstructorPrivado {
+
+}
--- proyectofinal/src/main/java/practica9/Singleton.java
+++ proyectofinal/src/main/java/practica9/Singleton.java
@@ -3,7 +3,18 @@ package practica9;
 public class Singleton {
     private static Singleton instance = null;
 
+    @ConstructorPrivado
     private Singleton(){
+    }
+
+    public static Singleton getInstance(){
+        if (instance == null) 
+            instance = new Singleton();
+        return instance;
+    }
+
+    protected  static  void log (String mensaje){
+        System.out.println("[LOG SYSTEM]: " + mensaje);
     }
 
 }
