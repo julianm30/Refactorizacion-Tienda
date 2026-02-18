9ef932c9f3573c81ab910d409da20e9998dc4676
julianm30 <jmonesp768@g.educaand.es>
2026-02-18 23:38:50 +0100

    todo hecho

--- proyectofinal/src/main/java/practica9/Tienda.java
+++ proyectofinal/src/main/java/practica9/Tienda.java
@@ -20,6 +20,11 @@ public class Tienda {
     public static ArrayList<Double> precios = new ArrayList<>(); // Precios
     public static ArrayList<Integer> stock1 = new ArrayList<>(); // Stock
 
+    // hago constantes con los numeros fijos
+    private static final double precioParaDescuento = 50.0;
+    private static final double descuentoDiezPorCiento = 0.9;
+    private static final int pocoStock = 3;
+
     private static Scanner sc = new Scanner(System.in);
 
     // Productos iniciales
@@ -58,7 +63,7 @@ public class Tienda {
         System.out.println("El producto se ha añadido correctamente");
     }
 
-    //Método para enseñar el inventario
+    // Método para enseñar el inventario
     private static void mostrarInventario() {
         if (nombres.isEmpty()) {
             System.out.println("No hay productos.");
@@ -66,26 +71,66 @@ public class Tienda {
 
         for (int i = 0; i < nombres.size(); i++) {
             System.out.println(i + ". " + nombres.get(i) + " - " +
-            precios.get(i) + "€ - Stock: " + stock1.get(i));
+                    precios.get(i) + "€ - Stock: " + stock1.get(i));
         }
     }
 
-
-    private static void gestionarVenta(){
+    // Método para hacer una venta
+    private static void gestionarVenta() {
         System.out.print("Introduzca nombre del producto a vender: ");
-                String prod = sc.next();
+        String productoVenta = sc.next();
 
-                int pos = -1;
-                for (int i = 0; i < nombres.size(); i++) {
-                    if (nombres.get(i).equalsIgnoreCase(prod)) {
-                        pos = i;
-                        break;
-                    }
-                }
+        int indice = buscarProducto(productoVenta);
+        if (indice == -1) {
+            System.out.println("Error: Producto no encontrado.");
+        }
+
     }
 
-    private static void buscarProducto(){
-        
+    // Método para buscar el producto en stock
+    private static int buscarProducto(String nombre) {
+        for (int i = 0; i < nombres.size(); i++) {
+            if (nombres.get(i).equalsIgnoreCase(nombre)) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    /**
+     * Hace el calculo del precio, aplica los descuentos y actualiza el stock.
+     * @param indice   Es la posicion del producto en las lsitas
+     */
+    private static void gestionarStock(int indice) {
+        System.out.println("Producto encontrado: " + nombres.get(indice));
+        System.out.println("Precio: " + precios.get(indice) + "€ | Stock: " + stock1.get(indice));
+        System.out.print("Cantidad a comprar: ");
+
+        int cantidad = sc.nextInt();
+        int stockVenta = stock1.get(indice);
+
+        if (cantidad > stockVenta) {
+            System.out.println("Error: No hay suficiente stock.");
+        }
+
+        // calculo el precio total
+        double precioTotal = cantidad * precios.get(indice);
+
+        //Descuento
+        if (precioTotal > precioParaDescuento) {
+            System.out.println("¡Oferta! Descuento aplicado por compra superior a 50€");
+            precioTotal = precioTotal * descuentoDiezPorCiento;
+        }
+
+        //Actualizo el stock
+        stock1.set(indice, stockVenta - cantidad);
+            System.out.println("Venta realizada. Total a pagar: " + precioTotal + "€");
+
+        System.out.println("[LOG]: Venta registrada.");
+            if (stock1.get(indice) < 3) {
+                System.out.println("[LOG SYSTEM]: ALERTA DE STOCK BAJO para " + nombres.get(indice));
+                }
     }
 
     public static void main(String[] args) {
@@ -95,6 +140,9 @@ public class Tienda {
         // el control del bucle
 
         while (!salir) {
+
+            productosPrueba();
+
             System.out.println("\n--- TIENDA ---");
             System.out.println("1. Añadir producto");
             System.out.println("2. Mostrar inventario");
@@ -113,65 +161,15 @@ public class Tienda {
                     mostrarInventario();
 
                 case 3:
+                    gestionarVenta();
+
+                case 4:
+                    System.out.println("Saliendo...");
 
                 default:
+                    System.out.println("Opción no válida.");
                     break;
             }
-
-        
-            
-
-            } else if (opcion == 3) {
-                System.out.println("\n--- VENTA ---");
-                System.out.print("Introduzca nombre del producto a vender: ");
-                String prod = sc.next();
-
-                int pos = -1;
-                for (int i = 0; i < nombres.size(); i++) {
-                    if (nombres.get(i).equalsIgnoreCase(prod)) {
-                        pos = i;
-                        break;
-                    }
-                }
-
-                if (pos != -1) {
-                    System.out.println("Producto encontrado: " + nombres.get(pos));
-                    System.out.println("Precio: " + precios.get(pos) + "€ | Stock: " + stock1.get(pos));
-                    System.out.print("Cantidad a comprar: ");
-                    int cant = sc.nextInt();
-
-                    if (stock1.get(pos) >= cant) {
-                        double total = cant * precios.get(pos);
-
-                        // Hay números fijos que se utilizan en el código
-                        if (total > 50) {
-                            System.out.println("¡Oferta! Descuento aplicado por compra superior a 50€");
-                            total = total * 0.90;
-                        }
-
-                        stock1.set(pos, stock1.get(pos) - cant); // Actualizar stock
-                        System.out.println("Venta realizada. Total a pagar: " + total + "€");
-
-                        // Debería ser Singleton
-                        System.out.println("[LOG SYSTEM]: Venta de " + cant + "x " + nombres.get(pos) + " registrada.");
-                        if (stock1.get(pos) < 3) {
-                            System.out.println("[LOG SYSTEM]: ALERTA DE STOCK BAJO para " + nombres.get(pos));
-                        }
-
-                    } else {
-                        System.out.println("Error: No hay suficiente stock.");
-                    }
-                } else {
-                    System.out.println("Error: Producto no encontrado.");
-                }
-
-            } else if (opcion == 4) {
-                System.out.println("Saliendo...");
-                break;
-            } else {
-                System.out.println("Opción no válida.");
-            }
         }
-        sc.close();
     }
 }
